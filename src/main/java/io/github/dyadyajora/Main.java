43f5cba92ec4f0edc57ea946a37324d9f30875cb
dyadyaJora <idyadyajora@gmail.com>
2025-01-18 20:58:11 +0400

    init with required publishing gradle plugins

---
+++ src/main/java/io/github/dyadyajora/Main.java
@@ -0,0 +1,15 @@
+package io.github.dyadyajora;
+
+/**
+ * @author dyadyaJora on 17.01.2025
+ */
+public class Main {
+    /**
+     * Library method example
+     * @param name user name
+     * @return greeting message
+     */
+    public static String libraryMethod(String name) {
+        return String.format("Hello from library, %s!", name);
+    }
+}
